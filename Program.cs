e9d583246c0f0ce8dfcd8672faae17a8e9b74452
Irina147258 <39420172+Irina147258@users.noreply.github.com>
2018-05-19 09:44:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Irina ");
+            Console.WriteLine("Lomachinskaya ");
+            Console.WriteLine("Irina");
+            Console.WriteLine("Lomachinskaya");
+        }
+    }
+}
